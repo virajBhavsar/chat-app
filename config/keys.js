4a418bd179f77eef56e22be64531399fbd9d22fe
virajBhavsar <viraj1234567898676@gmail.com>
2021-11-11 15:14:34 +0530

    v1.1

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	mongoURI : "mongodb://localhost:27017/chatBackup",
+	//mongoURI : "mongodb://localhost:27017/chatBackup",
+	mongoURI : "mongodb+srv://virajBhavsar:<password>@cluster0.mupmz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 }
