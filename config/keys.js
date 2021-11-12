324192accfbc51e4153aff2e7fad6d89f52fe45d
Viraj bhavsar <61341972+virajBhavsar@users.noreply.github.com>
2021-11-12 12:15:59 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
 	//mongoURI : "mongodb://localhost:27017/chatBackup",
-	mongoURI : "mongodb+srv://virajBhavsar:PMBOSiurDnC7KGFc@cluster0.mupmz.mongodb.net/chatBackup?retryWrites=true&w=majority",
+	mongoURI : "mongodb+srv://virajBhavsar:nhZAGY26n9q46QOc@cluster0.mupmz.mongodb.net/chatBackup?retryWrites=true&w=majority",
 }
